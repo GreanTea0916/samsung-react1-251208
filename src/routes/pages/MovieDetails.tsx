9fd4dbf4d813bca3f5f0b04278d45107b265c1f1
ParkYoungWoong <thesecon@gmail.com>
2025-12-08 17:47:22 +0900

    2025-12-08 17:48

--- src/routes/pages/MovieDetails.tsx
+++ src/routes/pages/MovieDetails.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
+import { useQuery } from '@tanstack/react-query'
 
 export interface Movie {
   Title: string
@@ -40,14 +41,21 @@ export default function MovieDetails() {
 
   useEffect(() => {
     async function fetchMovie() {
-      const { data: movie } = await axios.get(`https://omdbapi.com?apikey=7035c60c&i=${movieId}`)
+      const { data: movie } = await axios.get(
+        `https://omdbapi.com?apikey=7035c60c&i=${movieId}`
+      )
       setMovie(movie)
     }
     fetchMovie()
   }, [movieId])
 
-  return <>
-    <h1>{movie?.Title}</h1>
-    <img src={movie?.Poster} alt="" />
-  </>
+  return (
+    <>
+      <h1>{movie?.Title}</h1>
+      <img
+        src={movie?.Poster}
+        alt=""
+      />
+    </>
+  )
 }
--- src/stores/count.ts
+++ src/stores/count.ts
@@ -1,29 +1,42 @@
 import { create } from 'zustand'
-import { combine, subscribeWithSelector, persist } from 'zustand/middleware'
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  devtools
+} from 'zustand/middleware'
 
 export const useCountStore = create(
-  persist(
-    subscribeWithSelector(
-      combine(
-        {
-          count: 0,
-          double: 0 // count * 2
-        },
-        (set, get) => {
-          return {
-            increase: () => {
-              const { count } = get()
-              set({
-                count: count + 1
-              })
+  devtools(
+    persist(
+      subscribeWithSelector(
+        combine(
+          {
+            count: 0,
+            double: 0 // count * 2
+          },
+          (set, get) => {
+            return {
+              increase: () => {
+                const { count } = get()
+                set({
+                  count: count + 1
+                })
+              }
             }
           }
+        )
+      ),
+      {
+        name: 'countStore',
+        version: 1,
+        partialize: state => {
+          return {
+            count: state.count
+          }
         }
-      )
-    ),
-    {
-      name: 'countStore'
-    }
+      }
+    )
   )
 )
 
